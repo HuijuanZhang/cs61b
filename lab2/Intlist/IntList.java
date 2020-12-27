3dd0206e22faaffa5325f27e33f9c9cfc3da3f59
HuijuanZhang <huijuanzhang13@gmail.com>
2020-12-27 18:43:54 -0500

    modify catenate

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -100,7 +100,10 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        if(A.rest == null) {
+        if(A == null) {
+            return B;
+        }
+        else if(A.rest == null) {
             return new IntList(A.first, B);
         }
         return new IntList(A.first, catenate(A.rest, B));
